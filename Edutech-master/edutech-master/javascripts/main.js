daad42ed1f4c7ccb3bf5f150296e2f8b70e4c163
Richard-Aroh <109518710+Richard-Aroh@users.noreply.github.com>
2022-08-08 15:39:31 +0100

    Add files via upload

---
+++ Edutech-master/edutech-master/javascripts/main.js
@@ -0,0 +1,13 @@
+// Sticky Navbar
+const navbar = document.querySelector("nav");
+let sticky = navbar.offsetTop;
+const navbarScroll = () => {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+window.onscroll = navbarScroll;
+
+
